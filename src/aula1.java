7ee8925fc1f170e2d3531cde49915fc336641f9a
maria luiza <malufmatos22@gmail.com>
2024-10-01 20:02:07 -0300

    first commit

---
+++ src/aula1.java
@@ -0,0 +1,14 @@
+public class aula1 {
+    public static void main(String[] args) {
+        int xyz = 20;
+
+        int abc = 2;
+        int v = abc +1;
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        double d1 = 10.2;
+        double d2 = 20.5;
+    }
+}
