04f34d3490e079bdbfb839b9d7c461dacaee8aa0
maria luiza <malufmatos22@gmail.com>
2024-10-02 15:46:56 -0300

    aula 12

---
+++ src/aula12.java
@@ -0,0 +1,14 @@
+public class aula12 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá,\n  tudo\n    bem?");
+
+        String s = """
+                Olá,
+                  tudo
+                    bem?
+                """;
+        System.out.println(s);
+    }
+}
