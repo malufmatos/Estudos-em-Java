e1bfd1341b278f09071eea772225ddefb32afb94
maria luiza <malufmatos22@gmail.com>
2024-10-01 22:56:44 -0300

    aula 3

--- src/aula3.java
+++ src/aula3.java
@@ -2,15 +2,15 @@ public class aula3 {
     public static void main(String[] args) {
 
         int abc;
-        int 0abc; //ERROR
+        //int 0abc; //ERROR
         int abc42;
         int $var;
         int _var;
         int va$r;
         int var__;
-        int xp#o; //ERROR
-        int class; //ERROR
-        int package; //ERROR
+        // int xp#o; //ERROR
+        // int class; //ERROR
+        // int package; //ERROR
 
         int House;
         int house; //Follows convention
