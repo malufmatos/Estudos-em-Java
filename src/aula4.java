387ebb4de5062f4ff36cae5b3d6a7010c15c116c
maria luiza <malufmatos22@gmail.com>
2024-10-01 22:57:01 -0300

    aula 4

---
+++ src/aula4.java
@@ -0,0 +1,14 @@
+public class aula4 {
+    public static void main(String[] args) {
+
+        int n = 476_879_386;
+        System.out.println(n); //476879386
+
+        double d = 4_76.55_4_54_5_6_6_34;
+        System.out.println(d); //476.5545456634
+
+        //int x1 = _10;
+        //int x2 = 10_;
+        //double x3 = 10_.3;
+    }
+}
