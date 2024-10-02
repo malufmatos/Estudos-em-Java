7ec183004a93377d788226ddb2f537da01f7e0ed
maria luiza <malufmatos22@gmail.com>
2024-10-01 22:57:07 -0300

    aula 5

---
+++ src/aula5.java
@@ -0,0 +1,15 @@
+public class aula5 {
+    public static void main(String[] args) {
+        long a = 20_000_000_000l;
+        double b = 465.687058;
+
+         int c = 0b1010110101;
+         System.out.println(c); //693
+
+         int d = 0x12AF_2020;
+         System.out.println(d); //313466912
+
+         int e = 0444;
+         System.out.println(e); //292
+    }
+}
