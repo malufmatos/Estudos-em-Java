0eace6a8105950fb4ac175e4a9953ed2f6dfa929
maria luiza <malufmatos22@gmail.com>
2024-10-01 22:57:22 -0300

    aula 8

---
+++ src/aula8.java
@@ -0,0 +1,15 @@
+public class aula8 {
+
+    public static void main(String[] args) {
+
+        int i = 200;
+        double d= i;
+
+        System.out.println(i); //200
+        System.out.println(d); //200.0
+
+        double d2 = 200;
+
+        long l = 500;
+    }
+}
