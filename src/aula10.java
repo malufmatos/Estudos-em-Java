4260a3a3192957fb8b24d75975c67c9e20ada64e
maria luiza <malufmatos22@gmail.com>
2024-10-01 22:57:38 -0300

    aula 10

---
+++ src/aula10.java
@@ -0,0 +1,21 @@
+public class aula10 {
+
+    public static void main(String[] args) {
+
+        char c1 = 'a';
+        System.out.println(c1); //a
+
+        //ASCII Table
+        int i1 = (int)c1;
+        System.out.println(i1); //97
+
+        char c2 = 'a' + 2;
+        System.out.println(c2); //c
+
+        char c3 = 49;
+        System.out.println(c3); //1
+
+        char c4 = (char)-14;
+        System.out.println(c4);
+    }
+}
