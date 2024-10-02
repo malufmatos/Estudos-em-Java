dd4abb2ee9e6e3aa0edc00a6c0b6d4e0782894f9
maria luiza <malufmatos22@gmail.com>
2024-10-01 21:12:03 -0300

    aula 3

---
+++ src/aula3.java
@@ -0,0 +1,24 @@
+public class aula3 {
+    public static void main(String[] args) {
+
+        int abc;
+        int 0abc; //ERROR
+        int abc42;
+        int $var;
+        int _var;
+        int va$r;
+        int var__;
+        int xp#o; //ERROR
+        int class; //ERROR
+        int package; //ERROR
+
+        int House;
+        int house; //Follows convention
+        int HOUSE; //Constants convention
+
+        int numberOfYears; //Camel-case
+        int number_of_years;
+
+
+    }
+}
