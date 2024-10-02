8b2bfc217aa95cd290c54164418392185e549a35
maria luiza <malufmatos22@gmail.com>
2024-10-02 15:24:28 -0300

    aula 11

---
+++ src/aula11.java
@@ -0,0 +1,12 @@
+public class aula11 {
+
+    public static void main(String[] args) {
+
+        String s = "abc";
+        String y = "FFF";
+        String z = s + y;
+        z = z.toLowerCase();
+
+        System.out.println(z); //abcfff
+    }
+}
