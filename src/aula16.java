dc7dbe9e24e055b95c699ef5418f5142ea5a9742
maria luiza <malufmatos22@gmail.com>
2024-10-03 16:56:21 -0300

    aula 16

---
+++ src/aula16.java
@@ -0,0 +1,17 @@
+public class aula16 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 10;
+        boolean r = a == b;
+        System.out.println(r); //true
+
+        int c = 20;
+        System.out.println(a==c); //false
+        System.out.println(a != c); //true
+        System.out.println(a > b); //false
+        System.out.println( a >= b);//true
+
+    }
+}
