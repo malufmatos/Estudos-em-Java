775a64f0f56f061152f523d69dbd083a14574627
maria luiza <malufmatos22@gmail.com>
2024-10-03 17:08:31 -0300

    aula 19

---
+++ src/aula19.java
@@ -0,0 +1,8 @@
+public class aula19 {
+
+    public static void main(String[] args) {
+
+        boolean r = 15 - 5 * 2 > (3 - 1) / 2 && 4 <= 2 * 2 || !(2 != 4);
+        System.out.println(r); //True
+    }
+}
