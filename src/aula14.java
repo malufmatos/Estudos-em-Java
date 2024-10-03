9805307bb0c9b3f53236335a9a73adb20cb5c2f7
maria luiza <malufmatos22@gmail.com>
2024-10-03 16:37:51 -0300

    aula 14

---
+++ src/aula14.java
@@ -0,0 +1,16 @@
+public class aula14 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 2;
+        int c = 5;
+        int d = a * b - c;
+        System.out.println(d); //15
+
+        int e = 4;
+        int f = 2;
+        int g = e * (f / e);
+        System.out.println(g); //0
+    }
+}
