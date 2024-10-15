ee9c947c44d7eceb6dcdb7209f6b4b59d3bf4364
maria luiza <malufmatos22@gmail.com>
2024-10-15 13:58:27 -0300

    aula 26

---
+++ src/aula26.java
@@ -0,0 +1,17 @@
+public class aula26 {
+    public static void main(String[] args) {
+        int month = 13;
+
+        int days = switch (month) {
+            case 1, 3, 5, 7, 8, 12 -> 31;
+            case 2 -> 28;
+            case 4, 6, 9, 11 -> 30;
+            default -> {
+                System.out.println("Inválido");
+                yield 0;
+            }
+        };
+
+        System.out.println(days + " dias");
+    }
+}
