69f9fc7e074b36a2e096ec8e95d1ca53ee7a4fbe
maria luiza <malufmatos22@gmail.com>
2025-01-02 20:28:23 -0300

    aula 31

---
+++ src/aula31.java
@@ -0,0 +1,17 @@
+public class aula31 {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+//        String text;
+//
+//        if (age >= 18){
+//            text = "Maior de idade";
+//        } else {
+//            text = "Menor de idade";
+//        }
+
+        String text = age >= 18 ? "Maior de idade" : "Menor de idade";
+        System.out.println(text);
+    }
+}
