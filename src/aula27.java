2ff9baa3d2712299889dd1907c716c1991cbe8d2
maria luiza <malufmatos22@gmail.com>
2025-01-02 19:59:24 -0300

    aula 27

---
+++ src/aula27.java
@@ -0,0 +1,16 @@
+public class aula27 {
+
+    public static void main(String[] args){
+
+        int a = 10;
+        int b;
+
+        if (a == 10){
+            b = 15;
+        } else {
+            b = 20;
+        }
+
+        System.out.println(b);
+    }
+}
