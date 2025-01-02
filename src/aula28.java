c9a6fc0a1e45ebe546da49269a64fb4d814e37ec
maria luiza <malufmatos22@gmail.com>
2025-01-02 20:04:57 -0300

    aula 28

---
+++ src/aula28.java
@@ -0,0 +1,13 @@
+public class aula28 {
+
+    public static void main(String[] args){
+        int x = 4;
+
+        if (x % 2 == 0){
+            x += 5;
+        } else{
+            x *= 2;
+        }
+        System.out.println("x = " + x);
+    }
+}
