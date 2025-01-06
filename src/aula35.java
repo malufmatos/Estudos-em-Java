8dded82c76fef65734c81c218d274572cb619a71
maria luiza <malufmatos22@gmail.com>
2025-01-06 11:23:26 -0300

    aula 35

---
+++ src/aula35.java
@@ -0,0 +1,16 @@
+public class aula35 {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while(i<200){
+            System.out.println(i);
+            i++;
+
+            if( i ==100){
+                break;
+            }
+        }
+        System.out.println("FIM");
+    }
+}
