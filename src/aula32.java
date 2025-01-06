89c0fcd6a17fc9bbcc1cb9cb76a8f00ce7c2c39a
maria luiza <malufmatos22@gmail.com>
2025-01-06 11:10:46 -0300

    aula 32

---
+++ src/aula32.java
@@ -0,0 +1,21 @@
+public class aula32 {
+
+    public static void main(String[] args){
+
+        System.out.println("WHILE");
+        int i = 0;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("FIM");
+
+        System.out.println("DO... WHILE");
+        int j = 0;
+        do{
+            System.out.println(j);
+            j++;
+        } while (j<= 10);
+        System.out.println("FIM");
+    }
+}
