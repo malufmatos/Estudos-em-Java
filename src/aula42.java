45bb5c52747b1a42d50c72202cd3b53df81f35e0
maria luiza <malufmatos22@gmail.com>
2025-01-08 12:45:01 -0300

    aula 42

---
+++ src/aula42.java
@@ -0,0 +1,14 @@
+package debuganddocs;
+
+public class aula42{
+
+    public static void main(String[] args){
+        int x = 2;
+        int y = x + 5;
+
+        while(y<10){
+            System.out.println(y);
+            y+=2;
+        }
+    }
+}
