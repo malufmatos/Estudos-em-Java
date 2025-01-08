28b795d48ce7f19bd740af6deb29f2539c484fcb
maria luiza <malufmatos22@gmail.com>
2025-01-08 12:47:39 -0300

    aula 43

--- src/aula42.java
+++ src/aula42.java
@@ -3,12 +3,21 @@ package debuganddocs;
 public class aula42{
 
     public static void main(String[] args){
+        //Comentário qualquer
         int x = 2;
         int y = x + 5;
+        //FIXME corrigir isso aqui
 
         while(y<10){
             System.out.println(y);
             y+=2;
+            //TODO voltar aqui depois
         }
+
+        /*dlkfjvmsdf
+        sdklfjmv
+        sdfkvjmsdf
+        sdfklvm
+         */
     }
 }
