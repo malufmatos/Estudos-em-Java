4fec980d09e6d7c0a46ebd2b4b5b6f8aac4dbb9e
maria luiza <malufmatos22@gmail.com>
2025-01-13 23:24:51 -0300

    aula 43

---
+++ src/aula43.java
@@ -0,0 +1,33 @@
+
+/**
+ *Classe do meu programa
+ *
+ * @author Maria Luiza
+ * @version 1.0
+ * @since 2025
+ */
+public class aula43 {
+
+        /**
+         * Método main.
+         * @param args Parâmetros de linha de comando.
+         */
+    public static void main(String[] args){ m1 ( 10);}
+
+    /**
+     * Este é um método
+     * @param x 0 boolean
+     */
+
+    private static void m1(int x){
+    }
+
+    /**
+     * Este é outro método
+     * @param n1 Número 1
+     * @param n2 Número 2
+     * @return Por enquanto, true
+     */
+
+    private static boolean n2(double n, double n2){return true;}
+}
